6793d41ef456b038e42189c65fbe9f566f332d19
Christian Todd <christian.todd7@gmail.com>
2023-12-29 18:29:20 -0800

    Add remap for moving lines

--- lua/chrstntdd/core/remap.lua
+++ lua/chrstntdd/core/remap.lua
@@ -4,3 +4,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- jump up and down keeping the cursor in the center of the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Move lines up and down together in visual line moe
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
