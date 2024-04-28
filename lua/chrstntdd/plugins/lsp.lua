950c3e43e9018683efe1d0f5f03fa5d4b25e5621
Christian Todd <christian.todd7@gmail.com>
2024-04-28 11:23:24 -0700

    Upgrade plugins + add Astro support

--- lua/chrstntdd/plugins/lsp.lua
+++ lua/chrstntdd/plugins/lsp.lua
@@ -221,5 +221,10 @@ return {
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
+
+		lspconfig["astro"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 	end,
 }
