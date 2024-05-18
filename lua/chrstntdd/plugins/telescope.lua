2f6aef972d698bb86c1e7934da790e69aa5d8670
Christian Todd <christian.todd7@gmail.com>
2024-05-17 23:08:07 -0700

    Broken this whole time eh? Dynamic ftw

--- lua/chrstntdd/plugins/telescope.lua
+++ lua/chrstntdd/plugins/telescope.lua
@@ -32,7 +32,7 @@ return {
 		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		vim.keymap.set("n", "<leader>fg", "<cmd>Telescope git_file<cr>", { desc = "Find string in git project" })
+		vim.keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Find string in git project" })
 		vim.keymap.set(
 			"n",
 			"<leader>fc",
