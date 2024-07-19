d596b20beb36fdf52a1a58f9715ce5ef55d285f4
Christian Todd <christian.todd7@gmail.com>
2024-07-18 18:23:29 -0700

    Fix trouble incorrect config and more snippets

--- lua/chrstntdd/plugins/harpoon.lua
+++ lua/chrstntdd/plugins/harpoon.lua
@@ -1,38 +1,38 @@
 return {
-	"ThePrimeagen/harpoon",
-	event = "VeryLazy",
-	config = function()
-		vim.keymap.set(
-			"n",
-			"<leader>ha",
-			"<cmd>lua require('harpoon.mark').add_file()<cr>",
-			{ desc = "Mark file with harpoon" }
-		)
+  "ThePrimeagen/harpoon",
+  event = "VeryLazy",
+  config = function()
+    vim.keymap.set(
+      "n",
+      "<leader>ha",
+      "<cmd>lua require('harpoon.mark').add_file()<cr>",
+      { desc = "Mark file with harpoon" }
+    )
 
-		vim.keymap.set(
-			"n",
-			"<leader>hui",
-			"<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-			{ desc = "Mark file with harpoon" }
-		)
+    vim.keymap.set(
+      "n",
+      "<leader>hui",
+      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+      { desc = "Launch the [h]arpoon [ui]" }
+    )
 
-		vim.keymap.set(
-			"n",
-			"<leader>hp",
-			"<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-			{ desc = "Navigate to previous mark" }
-		)
+    vim.keymap.set(
+      "n",
+      "<leader>hp",
+      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
+      { desc = "Navigate to previous mark" }
+    )
 
-		vim.keymap.set(
-			"n",
-			"<leader>hn",
-			"<cmd>lua require('harpoon.ui').nav_next()<cr>",
-			{ desc = "Navigate to next mark" }
-		)
+    vim.keymap.set(
+      "n",
+      "<leader>hn",
+      "<cmd>lua require('harpoon.ui').nav_next()<cr>",
+      { desc = "Navigate to next mark" }
+    )
 
-		local slots = { 1, 2, 3, 4, 5 }
-		for _, n in ipairs(slots) do
-			vim.keymap.set("n", "<leader>" .. n, "<cmd>lua require('harpoon.ui').nav_file(" .. n .. ")<cr>")
-		end
-	end,
+    local slots = { 1, 2, 3, 4, 5 }
+    for _, n in ipairs(slots) do
+      vim.keymap.set("n", "<leader>" .. n, "<cmd>lua require('harpoon.ui').nav_file(" .. n .. ")<cr>")
+    end
+  end,
 }
--- lua/chrstntdd/snippets/typescriptreact.lua
+++ lua/chrstntdd/snippets/typescriptreact.lua
@@ -7,90 +7,99 @@ local c = ls.choice_node
 local i = ls.insert_node
 
 local ts_tsx = {
-	snippet({ trig = "cl", desc = "Print to the console" }, {
-		t("console.log("),
-		i(1),
-		t(")"),
-		i(0),
-	}),
+  snippet({ trig = "cl", desc = "Print to the console" }, {
+    t("console.log("),
+    i(1),
+    t(")"),
+    i(0),
+  }),
 
-	snippet({ trig = "lmt", desc = "Lingui msg macro" }, {
-		t("_(`"),
-		i(1),
-		t("`)"),
-		i(0),
-	}),
+  snippet({ trig = "lmt", desc = "Lingui msg macro" }, {
+    t("_(`"),
+    i(1),
+    t("`)"),
+    i(0),
+  }),
 
-	snippet({ trig = "cn", desc = "className attribute as a string literal" }, {
-		t('className="'),
-		i(1),
-		t('"'),
-		i(0),
-	}),
-	snippet({ trig = "cn`", desc = "className attribute as a template string" }, {
-		t("className={`${"),
-		i(1),
-		t("}`}"),
-		i(0),
-	}),
-	snippet({ trig = "cnx", desc = "className attribute with clsx" }, {
-		t("className={clsx(["),
-		i(1),
-		t("])}"),
-		i(0),
-	}),
-	snippet({ trig = "pp", desc = "Pretty print recursively" }, {
-		t("console.dir("),
-		i(1),
-		t(", { depth: Infinity }"),
-		t(")"),
-		i(0),
-	}),
-	snippet(
-		{ trig = "ReL", desc = "A Remix loader" },
-		fmt(
-			[[
+  snippet({ trig = "cn", desc = "className attribute as a string literal" }, {
+    t('className="'),
+    i(1),
+    t('"'),
+    i(0),
+  }),
+  snippet({ trig = "cn`", desc = "className attribute as a template string" }, {
+    t("className={`${"),
+    i(1),
+    t("}`}"),
+    i(0),
+  }),
+  snippet({ trig = "cnx", desc = "className attribute with clsx" }, {
+    t("className={clsx(["),
+    i(1),
+    t("])}"),
+    i(0),
+  }),
+  snippet({ trig = "pp", desc = "Pretty print recursively" }, {
+    t("console.dir("),
+    i(1),
+    t(", { depth: Infinity }"),
+    t(")"),
+    i(0),
+  }),
+  snippet(
+    { trig = "ReL", desc = "A Remix loader" },
+    fmt(
+      [[
 export async function loader(args: LoaderFunctionArgs) {{
   return {returnType}
 }}
       ]],
-			{
-				returnType = c(1, {
-					t("json({ some: {ob: 'ject'} })"),
-					t("defer({ some: prom })"),
-				}),
-			}
-		)
-	),
-	snippet(
-		{ trig = "dc", desc = "JS[D]oc [C]omment" },
+      {
+        returnType = c(1, {
+          t("json({ some: {ob: 'ject'} })"),
+          t("defer({ some: prom })"),
+        }),
+      }
+    )
+  ),
+  snippet(
+    { trig = "dc", desc = "JS[D]oc [C]omment" },
 
-		fmt(
-			[[
+    fmt(
+      [[
 /**
  * @{tag}
  *  {content}
  */
       ]],
-			{ tag = i(1), content = i(0) }
-		)
-	),
+      { tag = i(1), content = i(0) }
+    )
+  ),
 
-	snippet(
-		{ trig = "imp", desc = "Import" },
-		fmt(
-			[[
+  snippet(
+    { trig = "imp", desc = "Import" },
+    fmt(
+      [[
 import {importClause} from "{moduleSpecifier}"
   ]],
-			{
-				importClause = c(1, {
-					t("* as "),
-					t("{{ }}"),
-				}),
-				moduleSpecifier = i(0),
-			}
-		)
-	),
+      {
+        importClause = c(1, {
+          t("* as "),
+          t("{{ }}"),
+        }),
+        moduleSpecifier = i(0),
+      }
+    )
+  ),
+
+  snippet({ trig = "esld", desc = "ESLint disable next line" },
+    t("// eslint-disable-next-line")
+  ),
+
+  snippet({ trig = "eslD", desc = "ESLint disable" },
+    t("/* eslint-disable */")
+  )
+
 }
 
 ls.add_snippets("typescript", ts_tsx)
